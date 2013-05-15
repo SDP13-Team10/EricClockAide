1a47ca1b8db5061b5725d0a9039a354b8716d3e8
Sachin Honnudike <shonnudi@student.umass.edu>
2013-05-15 10:32:48 -0400

    Modified clockaide.py, added new DB

--- clockaide.py
+++ clockaide.py
@@ -3,7 +3,7 @@ import string,time,datetime,serial,re, os
 #from ClockAideHelpers import *
 
 BaudRate = 9600
-keypadLocation = "/dev/ttyUSB4"
+keypadLocation = "/dev/ttyUSB0"
 motorLocation = "/dev/ttyACM0"
 
 keypad = serial.Serial(keypadLocation,BaudRate)
@@ -12,36 +12,48 @@ motor = serial.Serial(motorLocation,BaudRate)
 currentTime = ""
 id = ""
 mode = ""
-
-# time.sleep(2)
-# keypad.flush()
-# motor.flush()
-
-# mode = normal
+comm = ""
 
 modes =	("NORMAL",  "CHECK_ID",  "READ", "SET", "TEACHER")
 
 stuff =	{
+	'00'  : "NORMAL", \
+	'01'  : "CHECK_ID", \
+	'02'  : "READ",\
+	'03'  : "SET", \
+	'04'  : "TEACHER", \
+	'05'  : "GOOD", \
+	'06'  : "WRONG", \
+
+	'07'  : "WAKE_UP",\
+	'08'  : "GET_TIME", \
+	'09'  : "RESET", \
+	'10' : "SPEAK_TIME"
+	}
+MODES =	{
 	'0'  : "NORMAL", \
 	'1'  : "CHECK_ID", \
 	'2'  : "READ",\
 	'3'  : "SET", \
-	'4'  : "TEACHER", \
-	'5'  : "GOOD", \
-	'6'  : "WRONG", \
-	'7'  : "WAKE_UP",\
-	'8'  : "GET_TIME", \
-	'9'  : "RESET", \
-	'10' : "SPEAK_TIME"
+	'4'  : "TEACHER"
+	}
+
+COMMAND = {
+	'0'  : "GOOD", \
+	'1'  : "WRONG", \
+	'2'  : "WAKE_UP",\
+	'3'  : "GET_TIME", \
+	'4'  : "RESET", \
+	'5'  : "SPEAK_TIME"
 	}
 	
 command = {
-	"good"		: '5', \
-	"wrong"		: '6', \
-	"wake_up"	: '7',\
-	"get_time"	: '8',\
-	"reset"		: '9',\
-	"unknown"	: '10'
+	"good"		: '0', \
+	"wrong"		: '1', \
+	"wake_up"	: '2',\
+	"get_time"	: '3',\
+	"reset"		: '4',\
+	"speak_time"	: '5'
 }
 
 modeLookUp = {
@@ -77,12 +89,19 @@ def initialization():
 	mode = modes[0]
 	
 def normal():
+	global comm
+	print "in normal"
 	try:
-		comm = stuff[str(keypad.read())]		## use different method other than stuff dictionary
-		
+		print keypad.inWaiting()
+		comm = COMMAND[str(keypad.read())]		## use different method other than stuff dictionary
+		print "recieved" 
+		print comm
 		if comm == "SPEAK_TIME":
 			speakTime(nowHour(), nowMinute())
 			print(keypad.write(modeLookUp["normal"]))
+			keypad.flushInput()
+			comm = ""
+			print comm
 			return modes[0]
 		elif comm == "WAKE_UP":					## send check ID signal to keypad and motor
 			print(keypad.write(modeLookUp["check_id"]))
@@ -103,13 +122,13 @@ def checkID():
 		id = keypad.read(3)
 		name = namesID[id]		## replace this with function to check input ID vs. database
 		if name:
-			print(keypad.write('5'))			## Sends "Correct" Code to Keypad
+			print(keypad.write(command["good"]))			## Sends "Correct" Code to Keypad
 			time.sleep(2)
 			print(keypad.write(name))			## Sends Student Name to Keypad
 			
-			# begging logging information
+			# beginning logging information
 			
-		return modes[int(keypad.read())]		## return statements???
+			return modes[int(keypad.read())]		## return statements???
 		
 	except KeyError:	
 		print(keypad.write('6'))				## put some logging functionality
@@ -149,7 +168,7 @@ def set():
 
 	try:
 
-		comm = stuff[str(keypad.read())]
+		comm = COMMAND[str(keypad.read())]
 	
 		if comm == "GET_TIME":
 			motortime = getTimeFromMotor()
